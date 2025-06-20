e8c47e4a083df870e6200439e065ebb0477c2da3
rksab <ramansarabha@gmail.com>
2025-06-20 21:12:42 +0530

    message

--- models/person.js
+++ models/person.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-console.log('connecting to', url)
 //connect to database
 mongoose
   .connect(url)
